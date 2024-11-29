44378acb8c61b4e95bb438c92ef617ee4d4a00b4
kylewinell <kwinell2@gmail.com>
2024-11-28 19:16:52 -0500

    Puzzle 4 Solution

--- 2015/Puzzle 4/main.go
+++ 2015/Puzzle 4/main.go
@@ -4,10 +4,23 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	data := "abcdef609043"
-	hash := md5.Sum([]byte(data))
-	fmt.Println(hex.EncodeToString(hash[:]))
+	data := "bgvyzdsv"
+	answer := ""
+	i := 0
+	for answer == "" {
+		hash := md5.Sum([]byte(data + strconv.Itoa(i)))
+		if hex.EncodeToString(hash[:])[0:6] == "000000" {
+			answer = hex.EncodeToString(hash[:])
+			fmt.Println(answer)
+			fmt.Println(i)
+			break
+		}
+		i++
+
+	}
+
 }
